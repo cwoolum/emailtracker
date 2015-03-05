6bb1c313a47af1d8ef800ac0f81fa0b9a9edfbaf
Chris Woolum <christopher.woolum@gmail.com>
2015-03-04 20:40:08 -0800

    fix angular

--- EmailTrackerWeb/Angular/app.js
+++ EmailTrackerWeb/Angular/app.js
@@ -6,6 +6,7 @@ angular.module('emailTracker', []);
 Office.initialize = function (reason) {
     $(document).ready(function () {
         console.log('Office Initialized');
-        //angular.bootstrap($('#container'), ['emailTracker']);
+        angular.bootstrap($('#container'), ['emailTracker']);
+        console.log('Angular Initialized');
     });
 };
